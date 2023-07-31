52c09f28672929df7cf81116e8c5d81ab37d830d
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-07-31 01:58:17 -0700

    worked on README, and changed the API for wearther

--- commands/getforcast/getforcast.js
+++ commands/getforcast/getforcast.js
@@ -4,31 +4,6 @@ const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 require('dotenv').config();
 
-// let state = {
-// 	temp: 0,
-// 	name: ''
-// }
-
-// const getForcast = async () => {
-// 	console.log('getting forecast')
-
-// 	try {
-
-// 		let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/current?postal_code=98106&key=${process.env.WEATHERBIT_KEY}`)
-
-// 		// console.log(weatherData.data.data[0].temp)
-// 		state.temp = weatherData.data.data[0].temp
-// 		// console.log(state, '***')
-
-		
-// 	} catch (error) {
-		
-// 	}
-// }
-// getForcast();
-
-// console.log(state)
- 
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -40,19 +15,14 @@ module.exports = {
 			.setRequired(true)
 		),
 	async execute(interaction) {
-		console.log(interaction.options.get('zip_code').value)
     let zipCode = interaction.options.get('zip_code').value;
 
     try {
 
-      let res = await axios.get(`https://api.weatherbit.io/v2.0/current?postal_code=${zipCode}&key=${process.env.WEATHERBIT_KEY}`)
-
-      let weatherData = res.data.data[0];
-
-
-      console.log(weatherData)
-
-      let forecastMessage = `The weather forecast for zipcode ${zipCode} ${weatherData.city_name}, ${weatherData.state_code}}\n Temperature: ${weatherData.temp}°C\n Conditions: ${weatherData.weather.description}`
+      let res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_KEY}&q=${zipCode}&aqi=no
+      `)
+      let weatherData = res.data;
+      let forecastMessage = `The weather forecast for zipcode ${zipCode} ${weatherData.location.name}, ${weatherData.location.region}\nTemperature: ${weatherData.current.temp_f}°F\nConditions: ${weatherData.current.condition.text}`
   
       await interaction.reply(forecastMessage);
       
