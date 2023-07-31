c19bea2d968fd5e0ead288c9ee496ebc2be9218c
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-07-30 21:34:03 -0700

    server initial start and running with permissions

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 'use strict'
 
+const {Client , IntentsBitField} = require('discord.js');
+require('dotenv').config()
+
+
+const token = process.env.TOKEN;
+
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+});
+
+client.on('messageCreate', (message) => {
+  console.log(message)
+});
+
+
+client.on('ready', (c) => {
+  console.log(`The bot, ${c.user.tag}, is ready 👌`)
+});
+
+client.login(token);
