7979ab1c69cfce3efa5daeac7e5c59e879023670
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-07-30 21:37:44 -0700

    commented out some code descriptions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict'
 
+// deconstruct Client and intents permissions
 const {Client , IntentsBitField} = require('discord.js');
 require('dotenv').config()
-
-
 const token = process.env.TOKEN;
 
-
+// Initialize client with specific permissions
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -19,9 +18,10 @@ client.on('messageCreate', (message) => {
   console.log(message)
 });
 
-
+//Logging when bot is running
 client.on('ready', (c) => {
   console.log(`The bot, ${c.user.tag}, is ready 👌`)
 });
 
+//logs in server using token
 client.login(token);
