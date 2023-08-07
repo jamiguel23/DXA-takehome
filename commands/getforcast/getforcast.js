81be46a91fb9b634bb63b23242b7489bc4d8b2c9
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-08-07 09:09:21 -0700

    playing around

--- commands/getforcast/getforcast.js
+++ commands/getforcast/getforcast.js
@@ -16,7 +16,7 @@ module.exports = {
 			option.setName('zip_code')
 			.setDescription('Enter zip code')
 			.setRequired(true)
-		),
+      ),
 	async execute(interaction) {
     // Get the value of the zip code
     let zipCode = interaction.options.get('zip_code').value;
@@ -29,8 +29,8 @@ module.exports = {
       let forecastMessage = `The weather forecast for zipcode ${zipCode} ${weatherData.location.name}, ${weatherData.location.region}\nTemperature: ${weatherData.current.temp_f}°F\nConditions: ${weatherData.current.condition.text}`;
   
       // Reply to interaction
-      await interaction.reply(forecastMessage);
-      
+      await interaction.reply({ content: forecastMessage, ephemeral: true });
+
     } catch (error) {
       console.error(`Error fetching weather forecast: ${error.message}`);
       await interaction.reply('Oh no! Looks like there is a server side error. Please try again later.');
